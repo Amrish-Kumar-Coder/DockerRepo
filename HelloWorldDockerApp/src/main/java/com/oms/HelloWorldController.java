042f1a5ab76d84fd256b8045cbfb984a9fbd91af
Amrish Kumar <amrishk27@gmail.com>
2020-10-08 17:51:42 +0530

    Docker App

---
+++ HelloWorldDockerApp/src/main/java/com/oms/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.oms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/")
+	public String helloWorld() {
+		return "Hello World V1";
+	}
+
+}
